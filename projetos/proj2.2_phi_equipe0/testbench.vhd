ef2c89853459173a588271288dc5eb28f9038062
mercedesDiniz <mercy.diniz82@gmail.com>
2022-10-08 18:34:07 -0300

    proj2.2 - v1 (.)

--- projetos/proj2.2_phi_equipe0/testbench.vhd
+++ projetos/proj2.2_phi_equipe0/testbench.vhd
@@ -1,4 +1,6 @@
 -- Testbench
+library IEEE;
+use IEEE.STD_LOGIC_1164.all;
 
 entity testbench is
 end testbench;
@@ -9,7 +11,7 @@ architecture comportamento of testbench is
 		clk, rst, b: in std_logic;   		-- b => Indicam o acionamento do butão
 		s0, s1 : out std_logic				-- s0, s1 => Indicam o estado
  	);
-	end component
+	end component;
 	
 	signal clk, b, s0, s1 : std_logic := '0';
 	signal rst : std_logic := '1';
